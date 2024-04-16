6ff34eb119ef98c9bd86dd4d347b82ff3e2e189f
BaldaAzz <ruslanfedosvich2019@gmial.com>
2024-04-16 20:10:57 +0300

    Added task_13

---
+++ BaldaAzz/task_13/N_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace N_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> Add = (a, b) => a + b;
+            Func<int, int, int> Sub = (a, b) => a - b;
+            Func<int, int, int> Mul = (a, b) => a * b;
+            Func<int, int, int> Div = (a, b) =>
+            {
+                if (b == 0) throw new DivideByZeroException();
+                return a / b;
+            };
+
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Sub(1, 2));
+            Console.WriteLine(Mul(1, 2));
+            Console.WriteLine(Div(1, 2));
+        }
+    }
+}
---
+++ BaldaAzz/task_13/N_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace N_2
+{
+    delegate double Average(Func<double>[] delegates);
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Func<double>[] myDelegates = new Func<double>[4];
+            for (int i = 0; i < myDelegates.Length; i++)
+            {
+                myDelegates[i] = () => new Random().Next(15);
+            }
+
+            Average average = delegate (Func<double>[] delegates)
+            {
+                double sum = 0;
+                
+                foreach (var del in delegates)
+                {
+                    sum += del();
+                }
+                return sum / delegates.Length;
+            };
+
+            Console.WriteLine(average(myDelegates));
+        }
+    }
+}
